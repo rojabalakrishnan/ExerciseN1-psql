77d711e48e7ca0029bf67ad582df9c7ffc8041f8
rojabalakrishnan <rojabalakrishnan69@gmail.com>
2020-10-06 02:37:19 -0700

    Update remote heroku psql-db Url

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var pg = require('pg');
 const {Pool} = require("pg");
-var url='postgresql://localhost:5432/local_db';
+var url='postgres://fszzrarrjnfdql:9854ee3a622a74f5cc04459ea9af99c768ecc8dfe5916eafd8710294ce0a617e@ec2-18-211-86-133.compute-1.amazonaws.com:5432/d6h34f0ntnu06e';
 const pool = new Pool({
   connectionString: url,
 });
